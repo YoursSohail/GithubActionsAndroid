345bb2bea92b9825bcd84b72feaf08aad2ecf4de
Mohammed Sohail <yourssohail@gmail.com>
2023-08-08 02:02:52 +0530

    added baseline file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    subprojects {
+        detekt {
+            baseline = file("${rootProject.projectDir}/detekt-baseline.yml")
+        }
+    }
 }
 
 dependencies {
